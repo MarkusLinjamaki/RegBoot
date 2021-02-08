f366cbd93ac187880943772d4f46241ebf533da5
Markus <markus.linjamaki@tuni.fi>
2021-02-08 19:09:40 +0200

    Expanded functions to work with multinomal regrsesion models

--- regressionBootstrap.R
+++ regressionBootstrap.R
@@ -1,67 +1,76 @@
-library(boot)
-library(bootstrap)
-library(readxl)
-library(ggplot2)
 ## Bootsrap residuals in regression model 
 # @ param model: regressio model lm.function
 # @ param iterations: number of iterations
 ##
 # return: dataframe including simulated beta values
+
 resBoot <- function(model,iterations){
-  beta_0 <- rep(0,times = iterations)
-  beta_1 <- rep(0,times = iterations)
   betas <- as.numeric(coefficients(model))
-  residuals <- resid(lm_water)
+  residuals <- resid(model)
   n <- length(residuals)
-  C <- model.matrix(lm_water) # model in matrix form
-  
-  
+  C <- model.matrix(model) # model in matrix form
+  # simulated beta values
+  boot_Betas <- matrix(nrow = iterations, ncol = length(betas))
   for(i in 1:iterations){ 
-    # sampling w/ replacement from empirical residual dist
+    # residual sample with replacement
     y_residualSample <- sample(residuals,n,replace = T)
+    
+    # Bootstrap responses
     y_star <- C %*% betas + y_residualSample
+    
+    # bootstrap least-square estimates
     beta_star <- solve(t(C) %*% C) %*% t(C) %*% y_star
-    beta_0[i] <- beta_star[1]
-    beta_1[i] <- beta_star[2]
+    
+    for(a in 1:length(betas)){
+      boot_Betas[i,a] <- beta_star[a]
+    }
   }
-  betaStars <- data.frame(beta_0,beta_1)
+  return(boot_Betas)
 }
+
+values <- resBoot(lm_sepal,100)
+values
+mean(values[,1])
+mean(values[,2])
 
 ## Bootsrap pairs in regression model 
 # @ param model: regressio model lm.function
 # @ param iterations: number of iterations
 ##
 # return: dataframe including simulated beta values
+
 pairsBoot <- function(model, iterations){
-  beta_0 <- rep(0,times = iterations)
-  beta_1 <- rep(0,times = iterations)
   betas <- as.numeric(coefficients(model))
-  
-  # resampled {1,2,...,n}
   n <- length(model$residuals)
   i_set <- seq(1,n,1)
+  boot_Betas <- matrix(nrow = iterations, ncol = length(betas))
+  
   ## not sampled values
-  y_values <- as.matrix(model.frame(lm_water)[1])
-  c_values <- as.matrix(model.frame(lm_water)[2])
+  y_values <- as.matrix(model.frame(model)[1])
+  c_values <- as.matrix(model.frame(model)[,-1])
+  
   # sampled values
-  c_vec <- rep(0,times = n)
-  y_vec <- rep(0,times = n)
+  c_vec <- matrix(nrow = length(c_values), ncol = length(c_values[1]))
+  y_vec <- matrix(nrow = n)
   # for the C matrix
   ones <- rep(1,n)
-
+  
   for(a in 1:iterations){
     # for every iteration new sample
     i_bootSample <- sample(i_set,n,replace = T)
     for(i in 1:n){
       index <- i_bootSample[i]
-      c_vec[i] <- c_values[index]
       y_vec[i] <- y_values[index]
+      c_vec[i,] <- c_values[index,]
     }
     C <- cbind(ones,c_vec)
     beta_star <- solve(t(C) %*% C) %*% t(C) %*% y_vec
-    beta_0[a] <-beta_star[1]
-    beta_1[a] <-beta_star[2]
+    
+    for(o in 1:length(betas)){
+      boot_Betas[a,o] <- beta_star[o]
+    }
+    
   }
-  betaStars <- data.frame(beta_0,beta_1)
+  return(boot_Betas)
 }
 
